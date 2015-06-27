3d0050f0a35b919d988b770b7f43ff92c0459b02
Tamizhvendan S <tamizh88@gmail.com>
2015-06-27 16:22:58 +0530

    Adding JwtToken Issuer

--- SuaveRestApi/SuaveRestApi/App.fs
+++ SuaveRestApi/SuaveRestApi/App.fs
@@ -7,6 +7,17 @@ module App =
     open SuaveRestApi.Rest
     open Suave.Web
     open Suave.Http
+    open System.IdentityModel.Tokens
+    open System.IdentityModel
+    open Suave.Http.Applicatives
+    open Suave.Types
+    open System.Security.Claims
+
+    (*
+        http://bitoftech.net/2014/10/27/json-web-token-asp-net-web-api-2-jwt-owin-authorization-server/
+        https://github.com/AzureAD/azure-activedirectory-identitymodel-extensions-for-dotnet/blob/master/tests/System.IdentityModel.Tokens.Jwt.Tests/CreateAndValidateTokens.cs
+        http://code.tutsplus.com/tutorials/token-based-authentication-with-angularjs-nodejs--cms-22543
+    *)
 
     [<EntryPoint>]
     let main argv =    
@@ -29,8 +40,24 @@ module App =
             UpdateById = MusicStoreDb.updateAlbumById
             Delete = MusicStoreDb.deleteAlbum
             IsExists = MusicStoreDb.isAlbumExists
-        }
+        }   
 
-        startWebServer defaultConfig (choose [personWebPart;albumWebPart])
+        let handler = new JwtSecurityTokenHandler();
+        let payload = new JwtPayload()
+        let header = new JwtHeader()
+        
+
+       
+
+        
+        let security =  
+            choose [
+                path "/client/add" >>= POST >>= request (RestFul.getResourceFromReq<Security.ClientRequest> >> Security.createClient >> JSON)
+                path "/client/token" >>= POST >>= request (RestFul.getResourceFromReq<Security.TokenRequest> >> Security.createToken "Suave" >> JSON)
+            ]
+             
+        
+
+        startWebServer defaultConfig (choose [personWebPart;albumWebPart;security])
         0 // return an integer exit code
 
---
+++ SuaveRestApi/SuaveRestApi/Security.fs
@@ -0,0 +1,87 @@
+﻿namespace SuaveRestApi
+
+open System
+open System.Security.Cryptography
+open Microsoft.Owin.Security.DataHandler.Encoder
+open System.Collections.Generic
+
+module Security =     
+    open Thinktecture.IdentityModel.Tokens
+    open System.Security.Claims
+    open System.IdentityModel.Tokens
+
+    type Client = {
+        ClientId : string
+        Base64Secret : string
+        Name : string
+    }
+
+    type ClientRequest = {
+        Name : string
+    }
+
+    type TokenRequest = {
+        UserName : string
+        Password : string
+        ClientId : string
+    } 
+    
+
+    type Token = {
+        AccessToken : string
+        TokenType : string
+        ExpiresIn : float
+    }
+    with static member Empty = {AccessToken = ""; TokenType = ""; ExpiresIn = 0.} 
+
+    let private clientStorage = new Dictionary<string, Client>()
+
+    let createClient clientRequest = 
+        
+        let clientId = Guid.NewGuid().ToString("N")
+        let data = Array.zeroCreate 32
+        RNGCryptoServiceProvider.Create().GetBytes(data)
+        let base64Secret = data |> TextEncodings.Base64Url.Encode
+        let newAudience = {ClientId = clientId; Base64Secret = base64Secret; Name =  clientRequest.Name}
+        clientStorage.Add(clientId, newAudience)
+        newAudience
+
+    let getClient clientId =
+        
+        if clientStorage.ContainsKey(clientId) then 
+            Some clientStorage.[clientId] 
+        else
+            None
+        
+
+    let createIdentity claims =
+            let identity = new ClaimsIdentity("JWT")
+            claims |> Seq.map (fun x -> new Claim(fst x, snd x)) |> Seq.iter identity.AddClaim
+            identity
+
+    let getClaims userName =
+        seq {
+            yield (ClaimTypes.Name, userName)
+            if (userName = "Admin") then
+                yield (ClaimTypes.Role, "Admin")            
+        } |> Seq.map (fun x -> new Claim(fst x, snd x))
+
+    let hmacSigningCredentials (key: byte[]) = new HmacSigningCredentials(key)
+
+    let createToken issuer tokenRequest =
+        match getClient tokenRequest.ClientId with
+        | Some client ->
+            if (tokenRequest.UserName = tokenRequest.Password) then
+                let signingKey = client.Base64Secret |> TextEncodings.Base64Url.Decode |> hmacSigningCredentials
+                let issued = new Nullable<DateTime>(DateTime.UtcNow)
+                let expires = new Nullable<DateTime>(DateTime.UtcNow.AddMinutes(5.))
+                let claims = getClaims tokenRequest.UserName 
+                let token = new JwtSecurityToken(issuer, tokenRequest.ClientId, claims, issued, expires, signingKey)
+                let handler = new JwtSecurityTokenHandler()
+                let accessToken = handler.WriteToken(token)
+                {AccessToken = accessToken; TokenType = "bearer"; ExpiresIn = TimeSpan.FromMinutes(5.).TotalSeconds}
+
+            else Token.Empty 
+            
+        | None -> Token.Empty    
+
