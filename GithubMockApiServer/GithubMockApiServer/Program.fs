7ec11b1c836f9cdb777a0582350d4071a00a68b9
Tamizhvendan S <tamizh88@gmail.com>
2015-07-23 18:08:00 +0530

    Adding GithubMockServerApi sample

---
+++ GithubMockApiServer/GithubMockApiServer/Program.fs
@@ -0,0 +1,20 @@
+﻿open System.IO
+open Suave.Http
+open Suave.Http.Successful
+open Suave
+open Suave.Http.Applicatives
+open Suave.Web
+
+[<EntryPoint>]
+let main argv = 
+    let json fileName =
+        let content = File.ReadAllText fileName  
+        content.Replace("\r", "").Replace("\n","")
+        |> OK >>= Writers.setMimeType "application/json"      
+    
+    let user = pathScan "/users/%s" (fun _ -> "User.json" |> json)  
+    let repos = pathScan "/user/%s/repos" (fun _ -> "Repos.json" |> json)
+    let mockApi = choose [user;repos]
+
+    startWebServer defaultConfig mockApi          
+    0 
