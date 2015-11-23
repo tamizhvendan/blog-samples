8d9aaf7f6460ce7ac700d773d3abee40044d6f6e
Tamizhvendan Sembiyan <sembiyat@bavarajk-pc.abcoind.local>
2015-11-23 19:13:28 +0530

    Adding react-hello-world sample

---
+++ react-hello-world/src/client/app/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {render} from 'react-dom';
+import AwesomeComponent from './AwesomeComponent.jsx';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <p> Hello React!</p>
+        <AwesomeComponent />
+      </div>
+    );
+  }
+}
+
+render(<App/>, document.getElementById('app'));
