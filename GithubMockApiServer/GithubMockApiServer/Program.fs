48b0141307dd78b795196ec6523d34cd96692b71
Sembiyan Tamizhvendan <sembiyat@advisory.com>
2016-01-11 15:32:09 +0530

    Upgrading to Suave 1.0

--- GithubMockApiServer/GithubMockApiServer/Program.fs
+++ GithubMockApiServer/GithubMockApiServer/Program.fs
@@ -1,16 +1,16 @@
 ﻿open System.IO
 open Suave.Http
-open Suave.Http.Successful
 open Suave
-open Suave.Http.Applicatives
-open Suave.Web
+open Suave.Operators
+open Suave.Successful
+open Suave.Filters
 
 [<EntryPoint>]
 let main argv = 
     let json fileName =
         let content = File.ReadAllText fileName  
         content.Replace("\r", "").Replace("\n","")
-        |> OK >>= Writers.setMimeType "application/json"      
+        |> OK >=> Writers.setMimeType "application/json"      
     
     let user = pathScan "/users/%s" (fun _ -> "User.json" |> json)  
     let repos = pathScan "/users/%s/repos" (fun _ -> "Repos.json" |> json)
