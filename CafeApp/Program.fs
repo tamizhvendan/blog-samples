47b51d34d59d3ac612ff3e8a2b8fd1e8bea311cd
Tamizhvendan S <tamizh88@gmail.com>
2016-03-03 11:50:33 +0530

    Making Rx Subscription Async

--- CafeApp/Program.fs
+++ CafeApp/Program.fs
@@ -4,9 +4,18 @@ open Data
 open Suave.Web
 open Api
 open EventsStore
+open System.Reactive.Linq
+open System.Reactive.Subjects
+open System.Reactive.Concurrency
+
+let asyncSubscribe<'a> (subject: ISubject<'a>) (f : 'a -> unit) =
+  subject
+    .ObserveOn(Scheduler.Default)
+    .Subscribe f
+
 [<EntryPoint>]
-let main argv =
-    use subscription =
-      inMemoryEventStore.EventStream.Subscribe projectReadModel
+let main argv =     
+    let es = inMemoryEventStore.EventStream
+    use subscription = asyncSubscribe es projectReadModel
     startWebServer defaultConfig (api inMemoryEventStore)
     0 // return an integer exit code
