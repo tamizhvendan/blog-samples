853137e9015e4c9a8e34a565c1affef6fc7467b1
J. Preiss <joerg.preiss@slesa.de>
2023-05-12 11:42:06 +0200

    Use .NET6 for RxFSharp

--- RxFsharp/RxFsharp/Program.fs
+++ RxFsharp/RxFsharp/Program.fs
@@ -3,9 +3,12 @@ open Suave.Filters
 
 [<EntryPoint>]
 let main argv = 
-
+    let config = {
+        defaultConfig with
+             bindings = [ HttpBinding.createSimple HTTP "127.0.0.1" 8083 ]
+    }
     let webpart = pathScan "/api/profile/%s" ApiGateway.getProfile
 
-    startWebServer defaultConfig webpart
+    startWebServer config webpart
     
     0 // return an integer exit code
